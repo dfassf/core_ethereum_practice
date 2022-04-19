186cf7e4775155c4003cb1cc589bd8e9ae62164b
Shinwoo Um <dfassf@gmail.com>
2022-04-19 23:07:33 +0900

    code 4-24

--- code4-24.sol
+++ code4-24.sol
@@ -18,11 +18,20 @@ contract UnsecureGeneralWalletCompatibleToken3 {
     }
 
     function transfer(address _to, uint256 _value) public {
-        if(balanceOf[msg.sender] < _value) return; // 호출자 잔고가 거래량보다 적으면 취소
-        balanceOf[msg.sender] -= _value; // 그렇지 않으면 실행
-        if(balanceOf[_to] + _value < balanceOf[_to]) return; // 수금인의 잔고+ 거래량이 수금인의 잔고보다 작으면 취소(???)
-        // -> 이 부분은 오버플로우 방지용인데, 오버플로우-100이 잔고인 상태에서 500을 보내면 오버플로우가 발생, 돈이 공중분해됨.
-        balanceOf[_to] += _value; // 수금인의 잔고 플러스
+        if(balanceOf[msg.sender] < _value) return; 
+        if(balanceOf[_to] + _value < balanceOf[_to]) return; // 두 가지를 다 체크 후 실행
+        balanceOf[msg.sender] -= _value; 
+        balanceOf[_to] += _value; 
         emit Transfer(msg.sender, _to, _value);
+    }
+}
+
+contract CrowdFundwWithUnsecureToken {
+    UnsecureGeneralWalletCompatibleToken3 tokenReward;
+    uint transferCount;
+
+    function transfer() external {
+        transferCount ++;
+        tokenReward.transfer(msg.sender, 10);
     }
 }
