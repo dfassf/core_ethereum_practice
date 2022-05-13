0c720c6d7f3c3bc21b9bd0461ad0f5d768bd1a27
Shinwoo Um <dfassf@gmail.com>
2022-05-14 00:21:10 +0900

    4-31

--- code4-31s.sol
+++ code4-31s.sol
@@ -14,21 +14,13 @@ contract DaoFund {
     }
 
     function withdrawBalance() external {
-        uint amountToWithdraw = balanceOf[msg.sender];
-/*
+            uint amountToWithdraw = balanceOf[msg.sender];
 
-The following syntax is deprecated: 
-f.gas(...)(), f.value(...)() and (new C).value(...)().  
-
-Replace these calls by
-f{gas: ..., value: ...}() and (new C){value: ...}(). 
-
- */
-        // if(msg.sender.call )
-        // if(msg.sender.call.value(amountToWithdraw)() == false) { // 0.8 에선 오류..
-        if(msg.sender.call{value:amountToWithdraw}("") == false){
-            revert();
-        }
+            // if((msg.sender.call{value: amountToWithdraw}("")){
+            (bool success, bytes memory data) = msg.sender.call{value: amountToWithdraw}("");
+            if(!success){
+                revert();
+            }
         balanceOf[msg.sender] = 0;
     }
 }
