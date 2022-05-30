6018ebb001c148f22de4c8171f32cb9d352e4a32
Shinwoo Um <dfassf@gmail.com>
2022-05-30 22:09:45 +0900

    crowdfund_fail

---
+++ crowdfund/dapp/dapp/constant.js
@@ -0,0 +1,6 @@
+// [주의] 현재 연결된 Geth 네트워크에 배포된 계정으로 업데이트 하세요.
+exports.mainAddress = "0xAbdCf835743B239e0F971D63a897D6DaEb2065e9";                // 토큰 생성 계정
+exports.makerAddress = "0xBAb7c61995c070B26D6De3F7bBD6b95d79e479D4";               // 제작자 계정
+exports.eoaAddress = "0x7f7ff1Da3Ea6072D9BDC5cE29CeA17831ca9021c";                 // 참여자 계정
+exports.TokenContractAddress = "0x2eB981383d3e6e598ab83EBf0a85De3Cb9DFF7a7";       // 토큰 계정
+exports.crowdFundContractAddress ="0x7FA400C025F2A567fe00031059D0BfDA30C34FEF";    // 펀딩 계정
