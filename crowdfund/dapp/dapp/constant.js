da38f5958d35e88737fc8228ec7295a7de2b6dc0
Shinwoo Um <dfassf@gmail.com>
2022-06-03 22:15:53 +0900

    외않됀데

--- crowdfund/dapp/dapp/constant.js
+++ crowdfund/dapp/dapp/constant.js
@@ -1,6 +1,6 @@
 // [주의] 현재 연결된 Geth 네트워크에 배포된 계정으로 업데이트 하세요.
-exports.mainAddress = "0xAbdCf835743B239e0F971D63a897D6DaEb2065e9";                // 토큰 생성 계정
-exports.makerAddress = "0xBAb7c61995c070B26D6De3F7bBD6b95d79e479D4";               // 제작자 계정
-exports.eoaAddress = "0x7f7ff1Da3Ea6072D9BDC5cE29CeA17831ca9021c";                 // 참여자 계정
-exports.TokenContractAddress = "0x2eB981383d3e6e598ab83EBf0a85De3Cb9DFF7a7";       // 토큰 계정
-exports.crowdFundContractAddress ="0x7FA400C025F2A567fe00031059D0BfDA30C34FEF";    // 펀딩 계정
+exports.mainAddress = "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4";                // 토큰 생성 계정
+exports.makerAddress = "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2";               // 제작자 계정
+exports.eoaAddress = "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db";                 // 참여자 계정
+exports.TokenContractAddress = "0xd9145CCE52D386f254917e481eB44e9943F39138";       // 토큰 계정
+exports.crowdFundContractAddress ="0xa131AD247055FD2e2aA8b156A11bdEc81b9eAD95";    // 펀딩 계정
